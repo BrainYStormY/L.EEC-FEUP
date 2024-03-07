e09e2e28ac6ecdfa4b8d6903e2befebf6355ff02
Francisco Silva <86657333+BrainYStormY@users.noreply.github.com>
2024-03-07 09:18:08 +0000

    Update animal.cpp

--- ESDA (Data Structures and Algorithms)/TP03/ex03/animal.cpp
+++ ESDA (Data Structures and Algorithms)/TP03/ex03/animal.cpp
@@ -33,12 +33,56 @@ void Veterinario::setEspecialidade(string spec)
 	especialidade = spec;
 }
 
-Animal::Animal(string spec, bool sex, string nom, int idad)
+Animal::Animal(string spec, bool sex, string nom, int idad):especie(spec), sexo(sex), nome(nom)
 {
-	//unfinished
-	especie = spec;
-	sexo = sex;
-	nome = nom;
-	idade = idad;
+	setIdade(idad);
+	Veterinario vet;
+	setVeterinario(vet);
+}
 
+Animal::Animal(string spec, bool sex, string nom, int idad, Veterinario vet):especie(spec), sexo(sex), nome(nom)
+{
+	setIdade(idad);
+	setVeterinario(vet);
+}
+
+string Animal::getEspecie() const
+{
+	return especie;
+}
+
+bool Animal::getSexo() const
+{
+	return sexo;
+}
+
+string Animal::getNome() const
+{
+	return nome;
+}
+
+void Animal::setIdade(int idad)
+{
+	idade = idad;
+}
+
+int Animal::getIdade(void) const
+{
+	return idade;
+}
+
+void Animal::setVeterinario(Veterinario vet)
+{
+	veterinario = vet;
+}
+
+Veterinario Animal::getVeterinario() const
+{
+	return veterinario;
+}
+
+Animal::Animal(Animal &animal):especie(animal.especie), sexo(animal.sexo), nome(animal.nome)
+{
+	idade = animal.idade;
+	veterinario = animal.veterinario;
 }
