f9bc0d25ae4c27b57d69011b9fa0c74ddaab669f
Francisco Silva <86657333+BrainYStormY@users.noreply.github.com>
2024-03-31 18:03:54 +0100

    Delete PRO (Programming)  directory

--- PRO (Programming) /TP01/exp.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int den, r;
-    den=0;
-    r=10%den;
-    printf("%d\n",r);
-    return 0;
-}
--- PRO (Programming) /TP01/somar2.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a, b, sum;
-    printf("Enter two numbers to get the sum.\n");
-    scanf("%d%d",&a,&b);
-    sum=a+b;
-    printf("%d+%d=%d\n",a,b,sum);
-}
